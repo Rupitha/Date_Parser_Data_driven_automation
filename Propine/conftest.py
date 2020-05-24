a94e044470067059da6c6d592b69bbf97248d632
Rupitha <rupithamohandas@gmail.com>
2020-05-25 05:07:54 +0800

    Files has been modified

--- Propine/conftest.py
+++ Propine/conftest.py
@@ -12,4 +12,14 @@ def setup(request):
     #driver.close()
 @pytest.fixture(params=[(18-12-1994,18/12/1994)])
 def getdata(request):
+    Dict = {}
+
+    book = openpyxl.load_workbook("//home//rupz//Documents//inputs.xlsx")
+    sheet = book.active
+    cell = sheet.cell(row=1, column=2)
+    print(cell.value)
+    for i in range(2, sheet.max_row + 1):
+        print(sheet.cell(row=i, column=2).value)
+        # print(sheet.cell(row=i,column=sheet.max_column).value)
+        Dict[sheet.cell(row=1, column=2).value] = sheet.cell(row=i, column=2).value
     return request.param
--- Propine/test_dateParser.py
+++ Propine/test_dateParser.py
@@ -3,11 +3,11 @@ from utilities.BaseClass import BaseClass
 
 class TestDate(BaseClass):
 
-    def test_date_parser(self,setup,getdata):
+    def test_date_parser(self,setup):
         print("Date parser")
         print(self.driver.title)
         print(self.driver.current_url)
-        self.driver.find_element_by_name("date").send_keys(getdata)
+        self.driver.find_element_by_name("date").sendkeys()
         self.driver.find_element_by_xpath("//input[@type='submit']").click()
         print(self.driver.find_element_by_xpath("/html[1]/body[1]/div[2]/div[1]/div[2]").text)
 
